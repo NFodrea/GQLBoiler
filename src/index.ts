53287b1da949591912ca42b49812be3470ec16cc
Nathaniel Fodrea <nathaniel.fodrea@genesys.com>
2019-01-02 12:13:47 -0500

    fix TS rules

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,27 @@
 import {GraphQLServer} from 'graphql-yoga';
 
 // type refs <- schems
+const typeDefs = `
+    type Query {
+        hello: String!
+    }
+`
 
 // resolvers
+
+const resolvers = {
+    Query: {
+        hello() {
+            return 'hello world'
+        }
+    }
+}
+
+const server = new GraphQLServer({
+    resolvers,
+    typeDefs,
+})
+
+server.start(() => {
+    console.log("server started");
+});
